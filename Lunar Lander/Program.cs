867a67635dc0c64e6e80033a041d77b33d3a20f1
Pewewe2212 <petteri.viitalahde@gmail.com>
2025-02-11 12:03:27 +0200

    Lunar Lander done (i hope)
    I think it's done, but I'm not 100% sure if I missed something

--- Lunar Lander/Program.cs
+++ Lunar Lander/Program.cs
@@ -4,32 +4,46 @@ using Raylib_cs;
 
 namespace Lunar_Lander
 {
+    /// <summary>
+    /// Needed:
+    /// Make the ship collision end the game **
+    /// /// </summary>
     internal class Program
     {
         public class Game
         {
             public bool gameRunning = false;
 
-            public float landerP1x = 50;
+            public bool gameEnd = false;
+
+            public int fuel = 5000;
+
+            public float landerP1x = 200;
             public float landerP1y = 25;
 
-            public float landerP2x = 25;
+            public float landerP2x = 175;
             public float landerP2y = 75;
 
-            public float landerp3x = 75;
-            public float landerp3y = 75;
+            public float landerP3x = 225;
+            public float landerP3y = 75;
 
-            public Vector2 landerP1 = new Vector2(50, 25);
-            public Vector2 landerP2 = new Vector2(25, 75);
-            public Vector2 landerP3 = new Vector2(75, 75);
+            // the real numbers for the Vector2s are applied in Update(), so these are just placeholders
+            public Vector2 landerP1 = new Vector2(200, 175);
+            public Vector2 landerP2 = new Vector2(175, 225);
+            public Vector2 landerP3 = new Vector2(225, 225);
 
             public Vector2 boosterP1 = new Vector2(40, 70);
             public Vector2 boosterP2 = new Vector2(50, 120);
             public Vector2 boosterP3 = new Vector2(60, 70);
 
-            public float speed = 0;
-            public float startSpeed = 100;
-            public float acceleration = 5; // no fucking idea what I'm supposed to imput here
+            public float velocity = 0;
+            public float acceleration;
+            public float engineAcceleration = 30;
+            public float direction = 0;
+            public float engineDirection = -1;
+            public float gravity = 10;
+
+            public Rectangle landingPad = new Rectangle(50, 275, 300, 25);
 
             public void Start()
             {
@@ -45,7 +59,7 @@ namespace Lunar_Lander
 
             public void Init()
             {
-                Raylib.InitWindow(1000, 600, "Lunar Lander");
+                Raylib.InitWindow(400, 300, "Lunar Lander");
                 gameRunning = true;
             }
 
@@ -62,42 +76,79 @@ namespace Lunar_Lander
 
             public void Update()
             {
-                landerP1y += speed;
-                landerP2y += speed;
-                landerp3y += speed;
+                float deltaTime = Raylib.GetFrameTime();
 
-                landerP1y += (startSpeed + acceleration * Raylib.GetFrameTime());
+                acceleration = gravity;
 
-                if (Raylib.IsKeyDown(KeyboardKey.Space))
+                if (Raylib.IsKeyDown(KeyboardKey.Space) && fuel >= 0)
                 {
-                    landerP1y -= speed * 2;
-                    landerP2y -= speed * 2;
-                    landerp3y -= speed * 2;
-
+                    acceleration -= engineAcceleration;
+                    fuel -= 1;
                 }
 
-                landerP1 = new Vector2(landerP1x, landerP1y);
-                boosterP1 = new Vector2(landerP1x - 10, landerP1y + 35);
+                if (landerP2y <= 275)
+                {
+                    velocity += acceleration * deltaTime;
 
-                landerP2 = new Vector2(landerP2x, landerP2y);
-                boosterP2 = new Vector2(landerP2x + 25, landerP2y + 35);
+                    landerP1y += velocity * deltaTime;
+                    landerP2y += velocity * deltaTime;
+                    landerP3y += velocity * deltaTime;
 
-                landerP3 = new Vector2(landerp3x, landerp3y);
-                boosterP3 = new Vector2(landerp3x - 15, landerp3y - 5);
+                    landerP1 = new Vector2(landerP1x, landerP1y);
+                    boosterP1 = new Vector2(landerP1x - 10, landerP1y + 35);
+
+                    landerP2 = new Vector2(landerP2x, landerP2y);
+                    boosterP2 = new Vector2(landerP2x + 25, landerP2y + 35);
+
+                    landerP3 = new Vector2(landerP3x, landerP3y);
+                    boosterP3 = new Vector2(landerP3x - 15, landerP3y - 5);
+                }
+
+                if (Raylib.CheckCollisionPointRec(landerP2, landingPad))
+                {
+                    gameEnd = true;
+                }
+
+                Debug.WriteLine(velocity);
+
+                if (gameEnd == true)
+                {
+                    if (velocity <= 15)
+                    {
+                        Raylib.DrawText("You Won", 200, 30, 30, Color.Gold);
+                    }
+                    else
+                    {
+                        Raylib.DrawText("You Failed", 200, 30, 30, Color.Maroon);
+                        landerP1 = new Vector2(276, 276);
+                        landerP2 = new Vector2(500, 500);
+                        landerP3 = new Vector2(500, 500);
+                    }
+                }
+
             }
+
 
             public void DrawGame()
             {
                 Raylib.BeginDrawing();
                 Raylib.ClearBackground(Color.DarkGray);
 
-                //the booster    
-                Raylib.DrawTriangle(boosterP1, boosterP2, boosterP3, Color.Orange);
+                if (Raylib.IsKeyDown(KeyboardKey.Space) && fuel >= 0)
+                {
+                    //the booster    
+                    Raylib.DrawTriangle(boosterP1, boosterP2, boosterP3, Color.Orange);
+                }
+
                 //the lander
                 Raylib.DrawTriangle(landerP1, landerP2, landerP3, Color.Maroon);
 
+                // the fuel
+                Raylib.DrawRectangle(20, 20, 110, 30, Color.LightGray);
+                Raylib.DrawRectangle(25, 25, fuel / 50, 20, Color.Red);
+
                 //the landing pad
-                Raylib.DrawRectangle(800, 550, 300, 50, Color.White);
+                Raylib.DrawRectangle(50, 275, 300, 25, Color.White);
 
 
                 Raylib.EndDrawing();
